582dc4ce0558bf1c94dbbefec24be21b6f26ff67
Paul Mouzas <paul.mouzas@gmail.com>
2014-04-22 12:28:59 -0400

    Created product inventory

--- Classes/product_inventory.py
+++ Classes/product_inventory.py
@@ -6,75 +6,42 @@ which keeps track of various products and can sum up the inventory
 value.
 """
 
-class Product:
+class Product(object):
 
-    def __init__(self, price, pid, qty):
-        """
-        Class constructor that needs a price, a product id,
-        and quantity.
-        """
+    def __init__(self, price, id, qty, name):
         self.price = price
-        self.pid = pid
+        self.id = id
         self.qty = qty
-
-    def update_qty(self, qty, method='add'):
-        """
-        Updates the quantity of produts. By default, adds the
-        passed quantity. Pass method as 'subtract' to subtract
-        the quantity.
-        """
-        if method == 'add':
-            self.qty += qty
-        elif method == 'subtract':
-            self.qty = max(0, self.qty - qty)
-
-    def print_product(self):
-        """
-        Prints a single product.
-        """
-        print '%d\t%s\t%.02f each' % (self.pid, self.qty, self.price)
-
-class Inventory:
-
-    def __init__(self):
-        """
-        Initializes the class instance.
-        """
-        self.products = [] # list to hold all products
-
-    def add(self, product):
-        """
-        Adds a passed Product to the list of products.
-        """
-        self.products.append(product)
-
-    def print_inventory(self):
-        """
-        Prints the current inventory, and the total value
-        of products.
-        """
-        value = 0
-        for product in self.products:
-            print '%d\t%s\t%.02f each' % (product.pid,
-                                          product.qty,
-                                          product.price)
-            value += (product.price * product.qty)
-        print '\nTotal value: %.02f' % value
-
-if __name__ == '__main__':
-    p1 = Product(1.4, 123, 5)
-    p2 = Product(1, 3432, 100)
-    p3 = Product(100.4, 2342, 99)
-
-
-    i = Inventory()
-    i.add(p1)
-    i.add(p2)
-    i.add(p3)
-    i.print_inventory()
-
-    p1.update_qty(10)
-    i.print_inventory()
+        self.name = name
+        
+    def update(self, amount):
+        product += amount
+        
+class Inventory(object):
     
-    p1.update_qty(10, method='subtract')
-    i.print_inventory()
+    def __init__(self):
+        self.products = []
+        
+    def add(self, product):
+        self.products.append(product)
+        
+    def printInventory(self):
+        value = 0
+        for p in self.products:
+            print  p.name
+            print 'Price: $%.2f' % round(p.price, 2)
+            print 'ID: %d' % p.id
+            print 'Qty: %d\n' % p.qty
+            value += (p.price * p.qty)
+            
+        print 'Total value $%.2f' % round(value, 2)
+            
+if __name__ == '__main__':
+    skittles = Product(.99, 1, 5, 'Skittles')
+    MandMs = Product(.89, 2, 10, 'M&Ms')
+
+    stuff = Inventory()
+    stuff.add(skittles)
+    stuff.add(MandMs)
+    
+    stuff.printInventory()
