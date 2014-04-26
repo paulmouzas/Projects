ef3b4dfd03c934ca457e8db323d1620f27df7b04
Paul Mouzas <paul.mouzas@gmail.com>
2014-04-26 12:37:22 -0400

    Added page scraper

--- Web/page_scraper.py
+++ Web/page_scraper.py
@@ -1,33 +1,18 @@
-# -*- coding: cp1252 -*-
-"""
-Page Scraper - Create an application which connects to a
-site and pulls out all links, or images, and saves them to
-a list. Optional: Organize the indexed content and don’t
-allow duplicates. Have it put the results into an easily
-searchable index file.
-"""
-
-import urllib2
+import requests
 from bs4 import BeautifulSoup
 
+while True:
+    try:
+        url = raw_input('Enter a url: ')
+        r = requests.get(url)
+        break
+    except Exception:
+        print 'Please enter a valid url.'
+soup = BeautifulSoup(r.text)
 
-def print_list(stuff):
-    print '\n'.join(stuff)
-    print '\n====================\n'
-
-if __name__ == '__main__':
-    
-    url = raw_input('Enter a URL: ')
-
-    choice = input('What to scrape?\n1. Links\n2. Images\n3. Both\n')
-    
-    soup = BeautifulSoup(urllib2.urlopen(url).read())
-
-    if choice == 1 or choice == 3:
-        urls = [link.get('href') for link in soup.findAll('a')]
-        print 'URLs:'
-        print_list(urls)
-    if choice == 2 or choice ==3:
-        images = [image['src'] for image in soup.findAll("img")]
-        print 'Images:'
-        print_list(images)
+links = [link for link in soup.find_all('a')]
+images = [image for image in soup.find_all('img')]
+for link in links:
+    print link
+for image in images:
+    print image
